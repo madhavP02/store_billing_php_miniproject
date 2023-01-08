31fa428ae65f80a4a5f8ff67ae15887de5406616
Madhav <madhavpatil87009@gmail.com>
2023-01-08 21:48:59 +0530

    commit

---
+++ dbconn.php
@@ -0,0 +1,15 @@
+<?php
+// CONNECTING TO DATABASE
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+// CREATING A CONNECTION
+$conn = mysqli_connect($servername, $username, $password, $database);
+// CHECK CONNECTION
+if(!$conn) {
+die("Connection failed: ".mysqli_connect_error());
+} else {
+echo "Connected successfully!";
+}
+?>
